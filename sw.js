8795a37c0683496dfa3b0f307d4d974e4d3b5a8d
nikolazemskov <kol.zemsckov@yandex.ru>
2026-08-10 04:04:13 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE="kolyagram-v5";
+const CACHE="kolyagram-v6";
 const STATIC=[
   "./manifest.webmanifest",
   "./icon-180.png",
   "./icon-512.png",
   "./config.js",
+  "./kolyagram-logo.jpg",
   "./egypt_01.png","./egypt_02.png","./egypt_03.png","./egypt_04.png","./egypt_05.png","./egypt_06.png",
   "./egypt_07.png","./egypt_08.png","./egypt_09.png","./egypt_10.png","./egypt_11.png","./egypt_12.png",
   "./egypt_13.png","./egypt_14.png","./egypt_15.png","./egypt_16.png","./egypt_17.png","./egypt_18.png"
