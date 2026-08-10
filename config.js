e532c1b1b9048b5fce3d038f04ba787fca766638
nikolazemskov <kol.zemsckov@yandex.ru>
2026-08-10 05:22:43 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = "https://ixivlbooogerrgjsrmmy.supabase.co";
+export const SUPABASE_URL = "https://ixivlboooqerrgjsrmmy.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_1o0M4kVGHuOi-bjJczso-A_BGMelpXL";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="kolyagram-v7-1";
+const CACHE="kolyagram-v7-2";
 const STATIC=[
   "./manifest.webmanifest",
   "./icon-180.png",
